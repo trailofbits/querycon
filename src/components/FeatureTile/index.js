12407c459c8b34e0766d1167a7fa0996bf056363
Taylor Kearns <tkearns@gmail.com>
2018-02-01 16:53:06 -0500

    Remove "Learn More" links

--- src/components/FeatureTile/index.js
+++ src/components/FeatureTile/index.js
@@ -6,10 +6,6 @@ const FeatureTile = ({ children, title, url }) => (
   <div className={styles.featureTile}>
     <h3 className={styles.title}>{title}</h3>
     <p>{children}</p>
-    <a className={styles.learnMoreLink} href={url}>
-      Learn More
-      <span className={styles.arrow}>&#10140;</span>
-    </a>
   </div>
 )
 
