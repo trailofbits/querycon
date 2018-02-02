97ccbf3b54991ed9b70cbd3035264c8496f640f3
Taylor Kearns <tkearns@gmail.com>
2018-02-02 14:56:48 -0500

    Visual refinements

--- src/components/TextLogo/index.js
+++ src/components/TextLogo/index.js
@@ -3,11 +3,7 @@ import React from 'react'
 import styles from './text-logo.module.scss'
 
 const TextLogo = () => (
-  <div className={styles.textLogo}>
-    <img className={styles.img} />
-
-    <span className={styles.text}>QueryCon</span>
-  </div>
+  <div className={styles.textLogo}>QueryCon</div>
 )
 
 export default TextLogo
