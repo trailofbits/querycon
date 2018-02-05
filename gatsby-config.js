31506da6cbc1299c716e7e54dcc739c32e2059e9
Taylor Kearns <tkearns@gmail.com>
2018-02-05 15:52:39 -0500

    Favicons
    Resolves #12

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,25 @@ module.exports = {
     title: 'QueryCon 2018',
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass'
+    {
+      resolve: 'gatsby-plugin-favicon',
+      options: {
+        logo: './src/favicon.png',
+        injectHTML: true,
+        icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: false,
+        favicons: true,
+        firefox: true,
+        twitter: false,
+        yandex: false,
+        windows: false
+        }
+      }
+    },
+    { resolve: 'gatsby-plugin-react-helmet' },
+    { resolve: 'gatsby-plugin-sass' }
   ],
 }
