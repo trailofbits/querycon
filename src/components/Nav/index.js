e40425b7d895176229305ed32dada787163e0349
Taylor Kearns <tkearns@gmail.com>
2018-02-05 14:56:36 -0500

    Use Link in place of anchor tags

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
+import Link from 'gatsby-link'
 
 import styles from './nav.module.scss'
 
 const Nav = () => (
   <div className={styles.nav}>
-    <a className={styles.link} href="#speakers">Speakers</a>
-    <a className={styles.link} href="#location">Location</a>
-    <a className={styles.link} href="/conduct">Conduct</a>
-    <a className={styles.link} href="/faq">FAQs</a>
+    <Link className={styles.link} to="#speakers">Speakers</Link>
+    <Link className={styles.link} to="#location">Location</Link>
+    <Link className={styles.link} to="/conduct">Conduct</Link>
+    <Link className={styles.link} to="/faq">FAQs</Link>
   </div>
 )
 
