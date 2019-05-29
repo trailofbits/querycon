97887a14a98ec8e7f8b71e2045e1493d16019af3
Mike Myers <30631532+mike-myers-tob@users.noreply.github.com>
2019-05-29 14:14:52 -0700

    Add an FAQ with updated answers for this year's event.

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -10,23 +10,22 @@ import Question from '../components/Question'
 import styles from './faq.module.scss'
 import { DESKTOP_MIN_WIDTH, MOBILE_WIDTH } from '../util/constants'
 
-/* TODO: Update FAQ
   const questions = [
   {
-    questionLine1: 'When does the conference start on May 31st?',
-    questionLine2: 'When does it end on June 1st?',
+    questionLine1: 'When does the conference start on June 20th?',
+    questionLine2: 'When does it end on June 21st?',
     answer:
-      'On the first day, there will be check-ins and breakfast starting at 8:00 AM. Open remarks begin at 9:00 AM, and the first presentation will begin around 10:00 AM. The conference portion of the day will end around 4:30 PM, when people can make their way to the after-hours event, which will begin at 5:00 PM. On June 1st, presentations will be complete at noon, and our Community Day will commence after lunch, and wrap up at 4:00 PM.',
+      'On the first day, there will be check-ins and light breakfast starting at 8:30 AM. We will have a brief introduction at 9:30 AM, and the first presentation will begin around 10:00 AM. The conference portion of the day will end around 4:30 PM, when people can make their way to the after-hours event, which will begin at 5:00 PM. On June 21st, presentations will be complete at noon, and our Community Workshop will commence after lunch, and wrap up at 4:00 PM.',
   },
   {
     questionLine1: 'Is breakfast provided? What about lunch and dinner?',
     answer:
-      'We will provide a light breakfast on both days at 8:00 AM until 9:00 AM. We also provide a full lunch and snack throughout the day. Coffee, tea, and water are served throughout both conference days. Food and drink will be served at the after-hours social event at <a href="http://www.schroederssf.com/" target="_blank">Schroder\'s Restaurant</a>.',
+      'We will provide a coffee bar on both days from 8:30 AM until 9:00 AM. We also provide a light lunch and access to snacks throughout both conference days. Food and drink will be served at the after-hours social event at <a href="https://www.mailroomnyc.com/" target="_blank">Mailroom</a>.',
   },
   {
     questionLine1: 'Are there any ticket discounts available?',
     answer:
-      'In addition to our Early Bird special (Ends March 31st), we offer a group discount for those purchasing 5-10 tickets at once. No code is necessary for this, just choose that option when registering.',
+      'We offer several discounts, including a press pass (just choose this option when registering), an academic discount for students or faculty, and a group discount of 25% for those purchasing 5-10 tickets at once. Inquire with us at our contact email for the relevant discount codes to use when purchasing.',
   },
   {
     questionLine1: 'Can I buy a ticket for just one day?',
@@ -36,17 +35,17 @@ import { DESKTOP_MIN_WIDTH, MOBILE_WIDTH } from '../util/constants'
   {
     questionLine1: 'Will this event be recorded?',
     answer:
-      'Yes. The presentations as well as portions of the community day will be recorded and shared after the event.',
+      'Yes. The presentations as well as portions of the community workshop will be recorded and shared after the event.',
   },
   {
     questionLine1: 'Will there be a live stream of the event?',
     answer:
-      'No. There will not be a live stream of the event, but we will be posting recordings of the talks as well as interesting content from the community day after the event.',
+      'No. There will not be a live stream of the event, but we will be posting recordings of the talks as well as interesting content from the community workshop after the event.',
   },
   {
     questionLine1: 'Will you have any tutorial or workshop-only days?',
     answer:
-      'We will feature a Community Day that will be similar to a workshop style session at QueryCon2018. This will occur in the afternoon on the second day.',
+      'We will feature a Community Workshop that will be similar to the workshop session at QueryCon2018. This will occur in the afternoon on the second day.',
   },
   {
     questionLine1: 'Should I bring a laptop?',
@@ -56,33 +55,32 @@ import { DESKTOP_MIN_WIDTH, MOBILE_WIDTH } from '../util/constants'
   {
     questionLine1: 'How far away is the conference venue from the airport?',
     answer:
-      'Approximately 20 miles from the airport, in San Francisco’s Marina/Presidio district.',
+      'Approximately 14 miles from Newark Liberty International Airport, 16 miles from JFK Airport and 15 miles from LaGuardia Airport.',
   },
   {
     questionLine1: 'Is parking available at the venue?',
     answer:
-      'There is <a href="https://www.google.com/maps/place/Parking+Area/@37.8062268,-122.4484005,18.54z/data=!4m12!1m6!3m5!1s0x808586d4bce240b9:0x3e7cf9a5c17c2c57!2sLittle+Marina+Green+Picnic+Area!8m2!3d37.805771!4d-122.4479267!3m4!1s0x808586d35950766d:0xade0d65df8f7410!8m2!3d37.8065216!4d-122.4477419" target="_blank">a free parking lot</a> in the Little Marina Green picnic area (right next to the venue)',
+      'There is parking available at <a href="https://www.parkwhiz.com/p/new-york-parking/90-water-st/?venue_id=47752" target="_blank"> near the venue',
   },
   {
     questionLine1: 'Can I sponsor this conference?',
-    answer: 'We are not accepting any sponsors.',
+    answer: 'We are not accepting any additional sponsors at this time.',
   },
   {
     questionLine1: 'Can I submit a talk about a commercial product?',
     answer:
-      'No, our goal is to ensure all talks center around open-source tools that attendees can easily utilize all of the available tools and software discussed in our speaker’s presentations. Please refrain from submitting talks that advertise, demo or heavily discuss any Osquery commercial offerings.',
+      'No, our goal is to ensure all talks center around open-source tools that attendees can easily utilize all of the available tools and software discussed in our speaker’s presentations. Please refrain from submitting talks that advertise, demo or heavily discuss any osquery commercial offerings.',
   },
   {
     questionLine1: 'Is this an official Facebook conference?',
     answer:
-      'While core members of Facebook’s Osquery team are attending and giving great talks, the conference is organized by Kolide, Inc.',
+      'No. While members of Facebook’s osquery team are invited to attend and may be present, the conference is organized by Trail of Bits and Kolide.',
   },
   {
     questionLine1: 'Do you offer refunds for tickets?',
     answer: 'No. We do not offer partial or full refunds for tickets.',
   },
 ]
-*/
 
 class FAQ extends Component {
   componentDidMount() {
@@ -104,7 +102,6 @@ class FAQ extends Component {
           <MobileHeader />
         </MediaQuery>
 
-        {/* TODO: Update when FAQ are ready}
         <div className={styles.heroSection}>
           <div className={styles.header}>Frequently Asked Questions</div>
           <img alt="" className={styles.heroImage} src={require('./faq-image.svg')} />
@@ -114,21 +111,20 @@ class FAQ extends Component {
               out:
             </span>
             <Button
-              url="mailto:info@querycon.io"
+              url="mailto:mike.myers@trailofbits.com"
               className={styles.contactUsButton}
             >
               Contact Us
             </Button>
           </div>
         </div>
-        {*/}
 
         {/* TODO: delete me eventually */}
         <div className={styles.heroSection}>
           <div className={styles.header}>Coming Soon!</div>
         </div>
 
-        {/* TODO: Update when FAQ are ready}
+
         <div className={styles.questions}>
           {questions.map((question) => (
             <Question
@@ -154,11 +150,10 @@ class FAQ extends Component {
             <div>{"Don't"} hesitate to reach out and ask!</div>
           </div>
 
-          <Button url="mailto:info@querycon.io" className={styles.bottomButton}>
+          <Button url="mailto:mike.myers@trailofbits.com" className={styles.bottomButton}>
             Contact Us
           </Button>
         </div>
-        {*/}
 
         <Footer />
       </div>
