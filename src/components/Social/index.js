fd69245feb195832a5485b5e7d6031fbd402d347
alicia <159570+alicia@users.noreply.github.com>
2019-06-04 15:49:04 -0400

    Adds after party details

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -1,28 +1,19 @@
 import React from 'react'
-import styles from './index.module.scss';
-import DiningImage from '../../pages/dining.jpg';
+
+import afterPartyImage from 'assets/images/querycon-afterparty-web.jpg'
+
+import styles from './index.module.scss'
 
 const Social = ({ id }) => (
-  <section id={id} className={styles.social}
-  >
-  <h1>Social Event info: Coming Soon!</h1>
-  {/* TODO: Update when we firm up the 2019 afterparty plan}
-    <h1>Social Event at {`Schroeder's`}</h1>
+  <section id={id} className={styles.social}>
+    <h1>After Party</h1>
     <p>
-      <a href="http://www.schroederssf.com/" rel="noopener noreferrer" target="_blank">
-        <img alt="" src={DiningImage} className={styles.primaryImage} />
-      </a>
-      Trail of Bits will be hosting an after-hours event at {" "}
-      <a href="http://www.schroederssf.com/" rel="noopener noreferrer" target="_blank">{`Schroeder's`} Restaurant</a> {" "}
-      , sponsored by Carbon Black, from 5:00pm-8:00pm after the first day of QueryCon, on June 20th.
+      Trail of Bits will be hosting an after-hours event at
+      <a href="https://www.mailroomnyc.com" rel="noopener noreferrer" target="_blank" style={{marginLeft: 5}}>
+        The Mailroom
+      </a>.
+      <img className={styles.primaryImage} src={afterPartyImage} alt="" />
     </p>
-    <p className={styles.socialDescription}>
-      Food and beverages will be provided, so come grab a drink, enjoy some hearty
-      German-style fare, and hang out with your fellow osquery community.
-      Lyft codes will be provided to help get you from the conference venue
-      to the restaurant!
-    </p>
-  {*/}
   </section>
 )
 
